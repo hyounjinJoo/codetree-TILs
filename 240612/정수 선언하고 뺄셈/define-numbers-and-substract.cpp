19819fc680482c1131bc95d494062dc9e9810635
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-06-12 00:21:41 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240612/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int Number1 = 97;
+    int Number2 = 13;
+    
+    std::cout << Number1 << " - " << Number2 << " = " << Number1 - Number2 << std::endl;
+
+    return 0;
+}
