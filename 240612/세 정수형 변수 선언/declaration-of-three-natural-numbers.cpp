ffa5612c889b33fe54764ac8a43a92c5e71441f0
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-06-12 00:33:57 +0900

    Title: 세 정수형 변수 선언, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240612/세 정수형 변수 선언/declaration-of-three-natural-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int Num7 = 7;
+    int Num23 = 23;
+
+    std::cout << Num7 << " + " << Num23 << " = " << Num7 + Num23 << std::endl;
+    
+    return 0;
+}
