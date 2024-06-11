c7bdaff801a6bf4db57c1b8c89c6709472b9deeb
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-06-12 04:09:14 +0900

    Title: 문자열 두번 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240612/문자열 두번 출력하기/print-string-twice.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    string Input;
+
+    cin >> Input;
+
+    cout << Input << endl << Input;
+    
+    return 0;
+}
