5d81c1ea6dabc57211f53eae03d45424c4298a36
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-06-12 00:18:32 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240612/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Total days in Year" << std::endl;
+    std::cout << "365" << std::endl;
+    std::cout << "Circumference rate" << std::endl;
+    std::cout << "3.1415926535" << std::endl;
+
+    return 0;
+}
