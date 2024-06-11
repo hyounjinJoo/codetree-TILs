0b63581cb0cfd2e7363e24be48a9c0692e6eccb8
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-06-12 03:52:27 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240612/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a = 0.f;
+    cin >> a;
+
+    a *= 1.5f;
+
+    cout << fixed;
+    cout.precision(2);
+
+    cout << a;
+
+    return 0;
+}
