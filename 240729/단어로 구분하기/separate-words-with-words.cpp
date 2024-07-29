71a84eb5e390348ad78abf568056c0b262f51fda
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-07-29 20:09:25 +0900

    Title: 단어로 구분하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    string TempStr;
+    for(int Idx = 0; Idx < 10; ++Idx)
+    {
+        cin >> TempStr;
+        cout << TempStr << endl;
+    }
+
+    return 0;
+}
