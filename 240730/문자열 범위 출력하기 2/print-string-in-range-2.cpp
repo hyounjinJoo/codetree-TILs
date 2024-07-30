d01da6ed49b7af769e869c2eb2fc1da13f020eac
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-07-30 16:33:43 +0900

    Title: 문자열 범위 출력하기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240730/문자열 범위 출력하기 2/print-string-in-range-2.cpp
+++ 240730/문자열 범위 출력하기 2/print-string-in-range-2.cpp
@@ -9,7 +9,12 @@ int main()
     cin >> LoopLimit;
 
     auto StartPos = Temp.end() - 1;
-
+    int Length = Temp.size();
+    if(LoopLimit > Length)
+    {
+        LoopLimit = Length;
+    }
+    
     for(int Idx = 0; Idx < LoopLimit; ++Idx)
     {
         cout << *(StartPos - Idx);
