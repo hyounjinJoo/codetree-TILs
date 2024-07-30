7e6f7db60c0292dee692894174b25f67e9018f09
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-07-30 18:43:16 +0900

    Title: 문자열 추가하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/문자열 추가하기/add-spring.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    string OutputTarget;
+    cin >> OutputTarget;
+    OutputTarget += "Hello";
+
+    cout << OutputTarget;
+
+    return 0;
+}
