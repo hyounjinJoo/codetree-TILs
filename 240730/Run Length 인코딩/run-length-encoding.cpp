6a3c14cb93eb99af26eb5cfc374b3e3764e9ac43
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-07-30 16:47:40 +0900

    Title: Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240730/Run Length 인코딩/run-length-encoding.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+    string SourceStr;
+    cin >> SourceStr;
+
+    string DestStr;
+
+    int LengthCurrentAlphabet = 0;
+    char CurrentAlphabet = 0;
+    for(int Idx = 0; Idx < SourceStr.length(); ++Idx)
+    {
+        if(CurrentAlphabet != SourceStr[Idx])
+        {
+            if(0 == LengthCurrentAlphabet)
+            {
+                ++LengthCurrentAlphabet;
+            }
+            else
+            {
+                string Temp;
+                Temp = CurrentAlphabet + to_string(LengthCurrentAlphabet);
+                DestStr.append(Temp);
+                LengthCurrentAlphabet = 1;
+            }
+            
+            CurrentAlphabet = SourceStr[Idx];
+        }
+        else
+        {
+            ++LengthCurrentAlphabet;
+
+            if(Idx == SourceStr.length())
+            {
+                string Temp;
+                Temp = CurrentAlphabet + to_string(LengthCurrentAlphabet);
+                DestStr.append(Temp);  
+            }
+        }
+    }
+
+    cout << DestStr.length() << endl;
+    cout << DestStr;
+
+    return 0;
+}
