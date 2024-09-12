9f9a329ea0f8c09d478ee7d0bff9de5a19621537
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-09-12 15:59:21 +0900

    Title: 아스키코드 변환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240912/아스키코드 변환/convert-to-ascii.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    char InputAlphabet;
+    int InputNumber;
+
+    cin >> InputAlphabet;
+    cout << (int)InputAlphabet;
+
+    cout << " ";
+
+    cin >> InputNumber;
+    cout << (char)InputNumber;
+
+    return 0;
+}
