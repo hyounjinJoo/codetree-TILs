de280cbe1c366d6d7d4f227a9c624b391e59963a
Hyoun Jin Joo ( 주현진 ) <122258824+hyounjinJoo@users.noreply.github.com>
2024-11-21 13:31:03 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241121/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool IsPrime(int n)
+{
+    for(int i = 2; i < n; i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() 
+{
+    int a, b;
+    cin >> a >> b;
+
+    int Sum = 0;
+    for(int Number = a; Number <= b; ++Number)
+    {
+        if(IsPrime(Number))
+        {
+            Sum += Number;
+        }
+    }
+
+    cout << Sum << endl;
+
+    return 0;
+}
